340f65d7c8c700fea08e6e6b366d4d225f05cd78
purwowd <id.purwowd@gmail.com>
2025-10-07 13:23:11 +0700

    Fixed

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -5,9 +5,12 @@ python3 manage.py makemigrations && \
 python3 manage.py makemigrations StaticAnalyzer && \
 python3 manage.py migrate
 set +e
-python3 manage.py createsuperuser --noinput --email ""
+export DJANGO_SUPERUSER_USERNAME="${DJANGO_SUPERUSER_USERNAME:-mobsf}"
+export DJANGO_SUPERUSER_PASSWORD="${DJANGO_SUPERUSER_PASSWORD:-mobsf}"
+export DJANGO_SUPERUSER_EMAIL="${DJANGO_SUPERUSER_EMAIL:-}"
+python3 manage.py createsuperuser --noinput
 set -e
 python3 manage.py create_roles
 
 exec gunicorn -b 0.0.0.0:8000 "mobsf.MobSF.wsgi:application" --workers=1 --threads=10 --timeout=3600 \
-    --worker-tmp-dir=/dev/shm --log-level=citical --log-file=- --access-logfile=- --error-logfile=- --capture-output
+    --worker-tmp-dir=/dev/shm --log-level=critical --log-file=- --access-logfile=- --error-logfile=- --capture-output
